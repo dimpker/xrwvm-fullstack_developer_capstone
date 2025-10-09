ecba9a3d451fe58162fb9c5886fd98d2b4709305
dimpker <listenbote@gmail.com>
2025-10-09 21:52:18 +0100

    Fix PEP8 violations and clean up test files
    - Applied autopep8 fixes to restapis.py and urls.py
    - Manually fixed migration file formatting (line length, indentation, trailing whitespace)
    - Removed temporary test files (test_add_review.py, test_frontend_debug.html, test_sentiment.py, test_views.py)
    - All Django app files now pass flake8 linting

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -16,7 +16,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarMake',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True, primary_key=True,
+                    serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
                 ('description', models.TextField()),
                 ('country', models.CharField(blank=True, max_length=50)),
@@ -26,15 +28,34 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarModel',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True, primary_key=True,
+                    serialize=False, verbose_name='ID')),
                 ('dealer_id', models.IntegerField()),
                 ('name', models.CharField(max_length=100)),
-                ('type', models.CharField(choices=[('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon'), ('COUPE', 'Coupe'), ('CONVERTIBLE', 'Convertible'), ('HATCHBACK', 'Hatchback'), ('TRUCK', 'Truck')], default='SUV', max_length=15)),
-                ('year', models.IntegerField(default=2023, validators=[django.core.validators.MaxValueValidator(2023), django.core.validators.MinValueValidator(2015)])),
-                ('engine_size', models.CharField(blank=True, max_length=20)),
-                ('fuel_type', models.CharField(default='Gasoline', max_length=20)),
-                ('transmission', models.CharField(default='Automatic', max_length=20)),
-                ('car_make', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='djangoapp.carmake')),
+                ('type', models.CharField(
+                    choices=[('SEDAN', 'Sedan'), ('SUV', 'SUV'),
+                             ('WAGON', 'Wagon'), ('COUPE', 'Coupe'),
+                             ('CONVERTIBLE', 'Convertible'),
+                             ('HATCHBACK', 'Hatchback'),
+                             ('TRUCK', 'Truck')],
+                    default='SUV', max_length=15)),
+                ('year', models.IntegerField(
+                    default=2023, validators=[
+                        django.core.validators.MaxValueValidator(
+                            2023),
+                        django.core.validators.MinValueValidator(
+                            2015)
+                    ])),
+                ('engine_size', models.CharField(
+                    blank=True, max_length=20)),
+                ('fuel_type', models.CharField(
+                    default='Gasoline', max_length=20)),
+                ('transmission', models.CharField(
+                    default='Automatic', max_length=20)),
+                ('car_make', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    to='djangoapp.carmake')),
             ],
         ),
     ]
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,23 +11,25 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except BaseException:
         # If any error occurs
         print("Network exception occurred")
 # Add code for get requests to back end
+
 
 def analyze_review_sentiments(text):
     """
@@ -38,7 +40,7 @@ def analyze_review_sentiments(text):
         request_url = sentiment_analyzer_url + "analyze/" + text
         print("Analyzing sentiment for: {}".format(text))
         print("Request URL: {}".format(request_url))
-        
+
         response = requests.get(request_url, timeout=2)
         return response.json()
     except Exception as e:
@@ -46,31 +48,32 @@ def analyze_review_sentiments(text):
         # Use built-in sentiment analysis as fallback
         return analyze_sentiment_builtin(text)
 
+
 def analyze_sentiment_builtin(text):
     """
     Built-in sentiment analysis using keyword matching
     """
     text_lower = text.lower()
-    
+
     # Positive keywords
     positive_words = [
-        'excellent', 'great', 'good', 'amazing', 'wonderful', 'fantastic', 
+        'excellent', 'great', 'good', 'amazing', 'wonderful', 'fantastic',
         'awesome', 'love', 'perfect', 'best', 'outstanding', 'superb',
         'brilliant', 'remarkable', 'impressive', 'satisfied', 'happy',
         'pleased', 'recommend', 'quality', 'smooth', 'comfortable'
     ]
-    
-    # Negative keywords  
+
+    # Negative keywords
     negative_words = [
         'bad', 'terrible', 'awful', 'horrible', 'worst', 'hate', 'poor',
         'disappointing', 'unsatisfied', 'problem', 'issue', 'broken',
         'defective', 'unreliable', 'waste', 'regret', 'avoid', 'never',
         'useless', 'frustrating', 'annoying', 'expensive', 'overpriced'
     ]
-    
+
     positive_score = sum(1 for word in positive_words if word in text_lower)
     negative_score = sum(1 for word in negative_words if word in text_lower)
-    
+
     if positive_score > negative_score:
         sentiment = "positive"
         polarity = min(0.8, 0.3 + (positive_score * 0.1))
@@ -80,7 +83,7 @@ def analyze_sentiment_builtin(text):
     else:
         sentiment = "neutral"
         polarity = 0.0
-    
+
     return {
         "sentiment": sentiment,
         "polarity": polarity,
@@ -88,11 +91,12 @@ def analyze_sentiment_builtin(text):
         "method": "built-in"
     }
 
+
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except BaseException:
         print("Network exception occurred")
