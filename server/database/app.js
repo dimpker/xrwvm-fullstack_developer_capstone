ec6fc3595a78e07df4056f24d4eb20cf020f43a5
dimpker <listenbote@gmail.com>
2025-10-10 20:41:08 +0100

    Fix Node.js to bind to 0.0.0.0 for Railway deployment

--- server/database/app.js
+++ server/database/app.js
@@ -113,6 +113,6 @@ app.post('/insert_review', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on port ${port}`);
 });
